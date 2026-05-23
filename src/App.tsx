653728db56ee0f6b6a43c4c3a85b8f85ee2b08c6
sudhir kumar <sk2001may@mail.com>
2026-05-23 22:32:06 +0530

    All api  are working with video playing and search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import MoviePage from './components/MoviePage'
 
 const App = () => {
--- src/components/MoviePage.tsx
+++ src/components/MoviePage.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable no-useless-assignment */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useState } from "react";
-import image from "../assets/react.svg";
 import { IoSearch } from "react-icons/io5";
 import { IoHome } from "react-icons/io5";
 import { HiTrendingUp } from "react-icons/hi";
@@ -9,109 +8,117 @@ import { TbChartBarPopular } from "react-icons/tb";
 import { CgProfile } from "react-icons/cg";
 import { IoClose } from "react-icons/io5";
 import axios from "axios";
-import ReactPlayer from 'react-player';
+import ReactPlayer from "react-player/youtube";
+import image2 from "../assets/image.png";
+import tiger from "../assets/tiger.png";
+
 const MoviePage = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [loading, setLoading] = useState(true);
   const [movie, setMovie] = useState<any[]>([]);
-  const [input,setInput]=useState("")
+  //   const [input, setInput] = useState("");
   const [click, setClick] = useState(false);
-  const [popup, setPopup] = useState("");
+  const [popup, setPopup] = useState<any>("");
   const [search, setSearch] = useState("");
-  const [trailerKey, setTrailerKey] = useState('');
+  const [trailerKey, setTrailerKey] = useState("");
+  const [category, setCategory] = useState("");
+  const [profile, setProfile] = useState(false);
 
-  //   https://api.themoviedb.org/3/trending/{media_type}/{time_window}
-//   async function fetData() {
-//     const res = await axios.get(
-//       `https://api.themoviedb.org/3/trending/movie/week?api_key=91de401eef2d1e423503c9a061cb82a4&page=7`,
-//     );
-//     // console.log(res);
-//     setMovie(res.data.results);
-//   }
-
-//   useEffect(() => {
-//     if(search.trim()==""){
-//     fetData();
-// }
-//   }, []);
-
-  //   console.log(movie);
+  const api_key=import.meta.env.VITE_API_URL
+  console.log(api_key)
 
   async function handleClick(ele: any) {
+
     setClick(!click);
     setPopup(ele);
-    let url=`https://api.themoviedb.org/3/movie/${ele.id}/videos?api_key=91de401eef2d1e423503c9a061cb82a4`
-    const video=await axios.get(url)
-    // setTrailerKey(video.results.key)
-    console.log(video)
-    // console.log(video.data.results[0].key)
-    const trailer=video.data.results.find((vid)=>vid.type=="Trailer")
-    if(trailer){
-        setTrailerKey(trailer?.key)
-    }else{
-        // <p>NO trailer found</p>
+    const url = `https://api.themoviedb.org/3/movie/${ele.id}/videos?api_key=${api_key}`;
+    const video = await axios.get(url);
+    console.log(video);
+    const trailer = video.data.results.find(
+      (vid: any) => vid.type == "Trailer",
+    );
+    if (trailer) {
+      setTrailerKey(trailer?.key);
+    } else {
+      <p>NO trailer found</p>;
     }
-
-
   }
 
-  useEffect(()=>{
-   
-  },[input])
-
-
+  console.log(category);
   useEffect(() => {
     async function fetchSearch() {
-         let url=""
-    if(search.trim()!==""){
-     url=`https://api.themoviedb.org/3/search/movie?api_key=91de401eef2d1e423503c9a061cb82a4&query=${search}`
-    }else{
-      url=`https://api.themoviedb.org/3/trending/movie/week?api_key=91de401eef2d1e423503c9a061cb82a4&page=7`
-    }
-    const res=await axios.get(url)
+      let url = "";
+      if (search.trim() !== "") {
+        url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${search}`;
+      } else if (category == "popular") {
+        // console.log(category)
+        url = `https://api.themoviedb.org/3/tv/popular?api_key=${api_key}&query=${search}`;
+      } else if (category == "top") {
+        // console.log(category)
+        url = `https://api.themoviedb.org/3/tv/top_rated?api_key=${api_key}&query=${search}`;
+      } else {
+        url = `https://api.themoviedb.org/3/trending/movie/week?api_key=${api_key}&page=7`;
+      }
+      const res = await axios.get(url);
       console.log("resukt search");
       console.log(res.data.results);
-      setMovie(res.data.results)
-      setLoading(false)
-}
+      setMovie(res.data.results);
+      setLoading(false);
+    }
 
- const timer=setTimeout(() => {
-    fetchSearch()
-
+    const timer = setTimeout(() => {
+      fetchSearch();
     }, 1000);
 
-    return ()=>{
-        clearTimeout(timer)
-    }
-  }, [search]);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [search, category]);
 
-  if(loading){
+  //   console.log(profile)
+  function handlePopular() {
+    setCategory("popular");
+  }
+
+  if (loading) {
     return (
-        <div className="flex justify-center items-center h-[100vh] w-full">
+      <div className="flex justify-center items-center h-[100vh] w-full">
         <div className="loader">
-            <span></span>
+          <span></span>
         </div>
-        </div>
-    )
+      </div>
+    );
   }
 
   return (
     <div>
-      <div className=" bg-[#1c1e23] h-[100vh]">
+      <div className=" bg-[#1c1e23] relative h-[100vh]">
         <nav
           className="w-[100vw] hidden sm:flex  h-[10vh] flex border items-center 
       justify-between p-4 bg-[#020617] text-white"
         >
-          <div>MOVIE</div>
+          <img src={image2} className="w-[15%] h-[80%]" alt="" />
+
           <div>
-            <ul className="flex gap-3 justify-between w-[30vw] cursor-pointer ml-[-2em]">
-              <li className="hover:text-[#00ffff] {} text-[#00ffff] text-lg border-b">
+            <ul className="flex gap-3 justify-around w-[30vw] cursor-pointer ml-[-2em]">
+              <li
+                className={`hover:text-[#00ffff] ${category === "" ? "borber border-b-2 text-cyan-400" : "text-white"} text-[#00ffff] text-lg `}
+                onClick={() => setCategory("")}
+              >
                 Home
               </li>
-              <li className="hover:text-[#00ffff] hover:border-b text-lg">
-                Trending
+              <li
+                onClick={handlePopular}
+                className={`hover:text-[#00ffff] ${category === "popular" ? "borber border-b-2 text-cyan-400" : "text-white"} hover:border-b text-lg`}
+              >
+                Popular
               </li>
-              <li className="hover:text-[#00ffff] text-lg">Popular</li>
+              <li
+                className={`hover:text-[#00ffff] ${category === "top" ? "borber border-b-2 text-cyan-400" : "text-white"} text-lg`}
+                onClick={() => setCategory("top")}
+              >
+                Top
+              </li>
               {/* <li className="hover:text-[#00ffff] text-lg">Trending</li> */}
             </ul>
           </div>
@@ -125,14 +132,20 @@ const MoviePage = () => {
             <IoSearch className="ml-[-2em] text-xl" />
           </div>
           <div>
-            <img src={image} alt="" />
+            {/* <img src={image} alt="" /> */}
+            <CgProfile
+              className="text-4xl "
+              onClick={() => setProfile(!profile)}
+            />
           </div>
         </nav>
 
         <nav className="flex  w-full justify-center fixed z-6 bg-[#1c1e23] text-white ">
           <div className="h-[6vh] flex justify-center sm:hidden w-full bg-[#1c1e23] fixed z-50  ">
-            <div className="flex items-center justify-between  gap-2 w-full  z-60  ">
-              <h1 className="m-2">MOVIE</h1>
+            <div className="flex items-center justify-between border-b border-[#00e5ff] gap-2 w-full  z-60  ">
+              {/* <h1 className="m-2">MOVIE</h1> */}
+              <img src={image2} className="w-[15%] m-4 pt-2 h-[80%]" alt="" />
+
               {/* <h1>Movie Hub</h1> */}
               <IoSearch
                 className="mr-[1em]  text-xl "
@@ -147,21 +160,37 @@ const MoviePage = () => {
           >
             <div className="flex flex-col justify-center  w-[90vw] ">
               <ul className="flex justify-around items-center   cursor-pointer">
-                <li className="hover:text-[#00e5ff] text-[#00ffff] text-lg border-b">
-                  <IoHome className="text-[6vw]" />
+                <li className="hover:text-[#00e5ff] text-[#00ffff] text-lg ">
+                  <IoHome
+                    className={`text-[6vw] ${category === "" ? "borber border-b-2 text-cyan-400" : "text-white"}`}
+                    onClick={() => setCategory("")}
+                  />
                   {/* Home */}
                 </li>
-                <li className="hover:text-[#00e5ff] hover:border-b text-lg">
-                  <HiTrendingUp className="text-[6vw]" />
+                <li
+                  className={`hover:text-[#00e5ff] ${category === "popular" ? "borber border-b-2 text-cyan-400" : "text-white"} hover:border-b text-lg`}
+                >
+                  <HiTrendingUp
+                    className="text-[6vw]"
+                    onClick={handlePopular}
+                  />
                   {/* Trending */}
                 </li>
-                <li className="hover:text-[#00ffff] text-lg">
-                  <TbChartBarPopular className="text-[6vw]" />
+                <li
+                  className={`hover:text-[#00ffff] ${category === "top" ? "borber border-b-2 text-cyan-400" : "text-white"} text-lg`}
+                >
+                  <TbChartBarPopular
+                    className="text-[6vw]"
+                    onClick={() => setCategory("top")}
+                  />
                   {/* Popular */}
                 </li>
                 <li className="hover:text-[#00ffff] text-lg">
                   {" "}
-                  <CgProfile className="text-[6vw]" />
+                  <CgProfile
+                    className="text-[6vw]"
+                    onClick={() => setProfile(!profile)}
+                  />
                   {/* Trending */}
                 </li>
               </ul>
@@ -194,7 +223,12 @@ const MoviePage = () => {
             </div>
           )}
         </div>
-
+        {category == "popular" && search.trim() == "" && (
+          <h1 className="text-white pl-5 text-2xl mt-20 z-50">Popular</h1>
+        )}
+        {category == "top" && search.trim() == "" && (
+          <h1 className="text-white pl-5 text-2xl mt-20 z-50">Top</h1>
+        )}
         <div className="grid relative p-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 gap-y-4 gap-x-4 place-items-center bg-[#1c1e23]">
           {movie.map((ele) => (
             <div
@@ -215,7 +249,10 @@ const MoviePage = () => {
               {/* Pop up card */}
               {click && (
                 <div className="">
-                  <div className="h-20 w-20 flex flex-col  absolute fixed top-[10%] md:top-[5%] left-[10%] z-20">
+                  <div
+                    className="h-20 left-1/2 -translate-x-1/2 flex flex-col  absolute 
+                  fixed top-[15%]  z-20"
+                  >
                     <div className=" flex justify-center ">
                       <IoClose
                         onClick={() => setClick(false)}
@@ -224,18 +261,29 @@ const MoviePage = () => {
                     </div>
 
                     {click && (
-                      <div className="md:h-[80vh] p-8  border border-[#00E5ff] bg-[#1c1e23] shadow-[0_0_5px_rgba(0,229,255,0.4)] flex justify-center gap-20 md:gap-0 items-center flex-col w-[80vw] text-white z-20">
+                      <div className=" h-auto max-h-[90vh] max-w-[1100px] m-2 p-3   bg-[#1c1e23] shadow-[0_0_5px_rgba(0,229,255,0.4)] flex justify-center gap-20 md:gap-0 items-center flex-col w-[80vw] lg:w-[80vw] text-white z-20">
                         <div className="">
-                          {/* <img
-                            src={`https://image.tmdb.org/t/p/original${popup.backdrop_path}`}
-                            className="h w-full md:h-[65%] md:pt-[20%] object-cover p-2"
-                            alt=""
-                          /> */}
-                          
-                          
-                    
+                          <IoClose
+                            className="text-4xl z-20 hidden md:flex"
+                            onClick={() => setClick(!click)}
+                          />
+
+                          {trailerKey ? (
+                            <div className="relative pt-[56%]">
+                              <ReactPlayer
+                                url={`https://www.youtube.com/watch?v=${trailerKey}`}
+                                controls={true}
+                                width="100%"
+                                height="100%"
+                                className="md:h-[55%] absolute top-0 left-0 w-full"
+                              />
+                            </div>
+                          ) : (
+                            <p>No trailer available</p>
+                          )}
+
                           <div className="flex justify-around ">
-                            <div className="">
+                            <div className="md:w-[70vw] lg:w-[55vw] xl:w-[45vw] w-[70vw]">
                               <h1 className="text-cyan-400">{popup.title}</h1>
                               <h1 className="text-white ">
                                 <strong className="text-cyan-400">
@@ -260,12 +308,12 @@ const MoviePage = () => {
                               </div>
                             </div>
                             <div>
-                              <button
+                              {/* <button
                                 className="border p-2 bg-red-400 text-black font-bold "
                                 onClick={() => console.log("hello")}
                               >
                                 Watch Now
-                              </button>
+                              </button> */}
                             </div>
                           </div>
                         </div>
@@ -293,7 +341,7 @@ const MoviePage = () => {
             </div>
           ))}
         </div>
-        {trailerKey ? (
+        {/* {trailerKey ? (
             <div className="relative">
         <ReactPlayer 
           url={`https://www.youtube.com/watch?v=${trailerKey}`} 
@@ -305,7 +353,31 @@ const MoviePage = () => {
         </div>
       ) : (
         <p>No trailer available</p>
-      )}
+      )} */}
+        <div className="flex h-auto w-full justify-center items-center">
+          {profile && (
+            <div className=" bg-[#1c1e23] rounded-xl text-white h-[70vh] w-[80vw] fixed justify-center items-center z-50 flex absolute top-0  mt-25">
+              <div className="absolute top-0 left-0">
+                <IoClose
+                  className="text-4xl"
+                  onClick={() => setProfile(!profile)}
+                />
+              </div>
+              <div className="flex justify-center flex-col items-center">
+                <img
+                  src={tiger}
+                  alt=""
+                  className="rounded-full h-[200px] w-[200px]"
+                />
+                <h1 className="text-3xl text-align-center">Tiger</h1>
+                <h1>INDIAN</h1>
+                <h1 className="text-center">
+                  A Powerful tiger Known for his strength and speed
+                </h1>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
